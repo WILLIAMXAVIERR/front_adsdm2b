4755994435c594783a46dab12668c63dfcecedb7
rhuan faria <eurhuangabriel@gmail.com>
2024-08-23 22:46:38 -0300

    adicionando inputs(nome e matricula)

--- ATIVIDADE02/projeto-0/src/App.jsx
+++ ATIVIDADE02/projeto-0/src/App.jsx
@@ -9,108 +9,116 @@ function App() {
   return (
     <>
       <h1> Avaliação De Matemática - 5°ano A</h1>
+      <form id="meu-formulario">
+        <label htmlFor="nome_aluno">Nome Completo:</label>
+        <input type="text" name="nome" id="nome_aluno" placeholder="Digite seu nome completo" />
+        <button type="button" id="btn_nome">Enviar</button>
 
-      <div className='multipla_escolha'>
+        <label htmlFor="matricula_aluno">Matrícula:</label>
+        <input type="number" name="matricula" id="matricula_aluno" placeholder="Digite sua matrícula" />
+        <button type="button" id="btn_matricula">Enviar</button>
+      </form>
+
+      <div id='q1'>
+        <p className='titulo'>
+          Questão 1 - Qual o resultado da equação: 258 + 142?
+        </p>
         <ul>
-          <p className='titulo'>
-            Questão 1 - Qual o resultado da equação: 258 + 142?
-          </p>
-
-          <li>
+          <li id="a1">
             <input type="radio" name="questao1" id="alternativas_a1" value="400" />
             <label htmlFor="alternativas_a1">A) 400</label>
           </li>
-          <li>
+          <li id="b2">
             <input type="radio" name="questao1" id="alternativas_b2" value="300" />
             <label htmlFor="alternativas_b2">B) 300</label>
           </li>
-          <li>
+          <li id="c1">
             <input type="radio" name="questao1" id="alternativas_c1" value="324" />
             <label htmlFor="alternativas_c1">C) 324</label>
           </li>
-          <li>
+          <li id="d1">
             <input type="radio" name="questao1" id="alternativas_d1" value="298" />
             <label htmlFor="alternativas_d1">D) 298</label>
           </li>
         </ul>
       </div>
 
-      <div className='multipla_escolha'>
+      <div id='q2'>
         <p className='titulo'>
-          Questão 2 - Qual tag é numera uma lista?
+          Questão 2 - Qual tag que enumera uma lista?
         </p>
         <ul>
-          <li>
+          <li id="a2">
             <input type="radio" name="questao2" id="alternativa_a2" value="li" />
             <label htmlFor="alternativa_a2">A) li</label>
           </li>
-          <li>
+          <li id="b2">
             <input type="radio" name="questao2" id="alternativa_b2" value="ul" />
             <label htmlFor="alternativa_b2">B) ul</label>
           </li>
-          <li>
+          <li id="c2">
             <input type="radio" name="questao2" id="alternativa_c2" value="ol" />
             <label htmlFor="alternativa_c2">C) ol</label>
           </li>
-          <li>
+          <li id="d2">
             <input type="radio" name="questao2" id="alternativa_d2" value="nl" />
             <label htmlFor="alternativa_d2">D) nl</label>
           </li>
         </ul>
       </div>
 
-      <div class='multipla_escolha'>
-        <p class="titulo">
+      <div id='q3'>
+        <p className='titulo'>
           Questão 3 - Em qual continente fica o Brasil?
         </p>
         <ul>
-          <li>
+          <li id="a3">
             <input type="radio" name="questao3" id="alternativa_a3" value="asia" />
-            <label htmlFor="asia">A) Ásia</label>
+            <label htmlFor="alternativa_a3">A) Ásia</label>
           </li>
-          <li>
+          <li id="b3">
             <input type="radio" name="questao3" id="alternativa_b3" value="africa" />
-            <label htmlFor="africa">B) África</label>
+            <label htmlFor="alternativa_b3">B) África</label>
           </li>
-          <li>
-            <input type="radio" name="questao3" id="alternativa_b4" value="europa" />
-            <label htmlFor="europa">C) Europa</label>
+          <li id="c4">
+            <input type="radio" name="questao3" id="alternativa_c4" value="europa" />
+            <label htmlFor="alternativa_c4">C) Europa</label>
           </li>
-          <li>
-            <input type="radio" name="questao3" id="alternativa" value="americaDoSul" />
-            <label htmlFor="americaDoSul">D) América do Sul</label>
+          <li id="d4">
+            <input type="radio" name="questao3" id="alternativa_d4" value="americaDoSul" />
+            <label htmlFor="alternativa_d4">D) América do Sul</label>
           </li>
         </ul>
       </div>
 
-      <div class='verdadeiro_ou_falso'>
-        <p class='titulo'>
+      <div id='q4'>
+        <p className='titulo'>
           Questão 4 - Computadores são feitos de adamantium.
         </p>
         <ul>
-          <li>
+          <li id="v1">
             <input type="radio" name="questao4" id="v1" value="verdadeiro" />
-            <label htmlFor="verdadeiro">Verdadeiro</label>
+            <label htmlFor="v1">Verdadeiro</label>
           </li>
-          <li>
+          <li id="f1">
             <input type="radio" name="questao4" id="f1" value="falso" />
-            <label htmlFor="falso">Falso</label>
+            <label htmlFor="f1">Falso</label>
           </li>
         </ul>
       </div>
 
-      <div class='verdadeiro_ou_falso'>
-        <p class='titulo'>
+      <div id='q5'>
+        <p className='titulo'>
           Questão 5 - Sorvete causa gripe.
         </p>
         <ul>
-          <li>
-            <input type="radio" name="questao5" id="v2" value="verdadeiro2" />
-            <label htmlFor="verdadeiro">Verdadeiro</label>
+          <li id="v2">
+            <input type="radio" name="questao5" id="v2" value="verdadeiro" />
+            <label htmlFor="v2">Verdadeiro</label>
           </li>
-          <li>
-            <input type="radio" name="questao5" id="f2" value="falso2" />
-            <label htmlFor="falso">Falso</label>
+          <li id="f2">
+            <input type="radio" name="questao5" id="f2" value="falso" />
+            <label htmlFor="f2">Falso</label>
           </li>
         </ul>
       </div>
